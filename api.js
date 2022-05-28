c52b06c0eb1fece4eb512a50839758a95324d698
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-05-28 09:35:42 -0500

    add endpoints, PUT PATCH DELETE

--- api.js
+++ api.js
@@ -10,6 +10,25 @@ app.post('/', (req, res) => {
   res.status(201).send('creando chanchito')
 })
 
+app.get('/:id', (req, res) => {
+  console.log(req.params)
+  res.status(200).send(req.params)
+})
+
+
+app.put('/:id', (req, res) => {
+  console.log(req.params)
+  res.sendStatus(204)
+})
+
+app.patch('/:id', (req, res) => {
+  res.sendStatus(204)
+})
+
+app.delete('/id', (req, res) => {
+  res.sendStatus(204)
+})
+
 app.listen(port, () => {
   console.log('starting the application')
 })
