7b56a0bfd9a864cb6a3c996e6f4fa7711188f552
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-05-28 13:13:25 -0500

    find one and create

--- user.controller.js
+++ user.controller.js
@@ -2,15 +2,18 @@ const Users = require('./User')
 
 const User = {
   get: async(req, res) => {
-    res.status(200).send('este es un chanchito')
+    const { id } = req.params
+    const user = await Users.findOne({ _id: id })
+    res.status(200).send(user)
   },
   list: async(req, res) => {
     const user = await Users.find()
     res.status(200).send(users)
   },
   create: async(req, res) => {
-    console.log(req.body)
-    res.status(201).send('creando un chanchito')
+    const user = new Users(req.body)
+    const saveUser = await user.save()
+    res.status(201).send(saveUser._id)
   },
   update: async(req, res) => {
     res.status(204).send('actualizando chanchito')
